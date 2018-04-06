fe1e2dd98711984389f6a980a30e5980091ff42a
Jake Hao <jhao@snapchat.com>
2018-04-06 18:34:08 +0800

    update style

--- site/client/src/index.jsx
+++ site/client/src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import api from './api'
 import styles from './styles.css'
 import { maybeEmailAddress } from './utils'
+import screenshot from './screenshot.png'
 
 class App extends React.Component {
   constructor () {
@@ -70,24 +71,30 @@ class App extends React.Component {
   render () {
     const { code, verified, email, loading } = this.state
 
+    document.body.style = 'background: #b8e5f8;';
+
     return (
       <div className={styles.MainContainer}>
-          <h2 className={styles.CenterText}>Yoo</h2>
-          <h4 className={styles.CenterText}>在 V2EX 认识好朋友</h4>
+          <h2 className={styles.CenterText}>本周对话</h2>
+          <h4 className={styles.CenterText}>轻松拓展你的朋友圈</h4>
           <div className={styles.FormContainer}>
+            1. 输入你的 V2EX 用户名
             <input
-              placeholder='v2ex username'
+              placeholder='用户名'
               type='text'
               className={styles.UserNameInput}
               onChange={this.onUserNameChange.bind(this)}
             />
+            2. 把下面的验证码添加到 V2EX 个人简介 (?)
             <input
-              placeholder='code'
+              placeholder='自动生成验证码'
               type='text'
+              disabled='true'
               className={styles.CodeInput}
               value={code}
             />
             <p> { loading }</p>
+            3. 输入你的邮箱
             <input
               placeholder='email'
               type='email'
@@ -100,8 +107,15 @@ class App extends React.Component {
               disabled={!verified && !!email}
               onClick={this.handleSubmit.bind(this)}
               className={styles.SubmitBtn}
-            > Submit
+            > 注册使用
             </button>
+          </div>
+          <div className={styles.introContainer}>
+              <p className={styles.CenterText}>· · ·</p>
+              <h4 className={styles.CenterText}>How does it work?</h4>
+              <p>本周会话每周为你匹配一位 V2EX 好友，然后发送一封包括他个人介绍的邮件到你的邮箱。</p>
+              <p className={styles.CenterText}>· · ·</p>
+              <img className={styles.Screenshot} src={screenshot} />
           </div>
       </div>
     )
