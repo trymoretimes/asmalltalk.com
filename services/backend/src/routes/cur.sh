cbc3c03f21a903739135e1fb8c6044896e8d6766
Minghe Huang <h.minghe@gmail.com>
2018-04-08 12:04:58 +0800

    use dynamic useragent
    Former-commit-id: a55e484453317c75d345fc479cb7a8dd9fd1a20f

---
+++ services/backend/src/routes/cur.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true;
+do
+  curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36"  https://www.v2ex.com/member/metrue | grep bz
+done
