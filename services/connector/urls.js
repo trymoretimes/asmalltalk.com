8ef2f9de662c03612bb383733217fbd2568af0bf
Minghe Huang <h.minghe@gmail.com>
2018-04-11 14:37:59 +0800

    clean up
    Former-commit-id: de2d90d38bf2ad4de9e10d1146f10fe2d5b85be2

--- services/connector/urls.js
+++
@@ -1,5 +0,0 @@
-https://yeaxt3l4hk.execute-api.us-east-1.amazonaws.com/production
-https://3gyn4qlhxd.execute-api.us-east-1.amazonaws.com/production
-https://ag04n2w1s5.execute-api.us-east-1.amazonaws.com/production
-https://bqnc005g2i.execute-api.us-east-1.amazonaws.com/production
-https://ftgvr8utp1.execute-api.us-east-1.amazonaws.com/production
