0fb78acb2299357ecf1321e0ed1c49cbf6ce7b55
Minghe Huang <huangminghe@juran.com.cn>
2018-04-13 21:13:27 +0800

    fix typo
    Former-commit-id: 775b02023f2154500c62b5502efad42af77215e3

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,5 +2,5 @@
 
 docker build -f devops/dockerfile.client -t ff-client .
 docker run -p 80:80 -d ff-client
-docker build -f devops/dockerfile.server -t ff-server
+docker build -f devops/dockerfile.server -t ff-server .
 docker run -p 5002:5002
