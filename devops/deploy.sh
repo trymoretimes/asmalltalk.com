e3b10fdfc4ce426c9c49832c7bb631d0d8b9742d
Minghe Huang <huangminghe@juran.com.cn>
2018-04-13 21:08:44 +0800

    update
    Former-commit-id: 98dd8740c2153d46e2c008317473f061f7f5ae1f

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-target='root@172.104.86.178'
-ssh -o "StrictHostKeyChecking no" ${target} <<END
-  docker rmi \$(docker images --filter "dangling=true" -q --no-trunc)
-  cd ~/ff
-  docker build -f devops/dockerfile.client -t ff-client .
-  docker run -p 80:80 -d ff-client
-  docker build -f devops/dockerfile.server -t ff-server
-  docker run -p 5002:5002
-END
+docker build -f devops/dockerfile.client -t ff-client .
+docker run -p 80:80 -d ff-client
+docker build -f devops/dockerfile.server -t ff-server
+docker run -p 5002:5002
