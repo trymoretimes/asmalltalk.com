849f4e3f7cf8fc53666a570b3cf9135e20b30bf4
Minghe Huang <h.minghe@gmail.com>
2018-04-14 00:18:03 +0800

    stop first
    Former-commit-id: 7ae9d9b341c6daa483dcd89d4a9a23b946c4ff41

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker stop $(docker ps -a -q)
 docker build -f devops/dockerfile.client -t ff-client .
 docker run -p 80:80 -d ff-client
 docker build -f devops/dockerfile.server -t ff-server .
