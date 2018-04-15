db14bacbda9fdc0bb6203e0129b8af99d02bb398
Minghe Huang <h.minghe@gmail.com>
2018-04-15 16:11:36 +0800

    update styles
    Former-commit-id: 26c35caa80045523367e3814b7b4ae2d95584641

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -113,7 +113,7 @@ class App extends React.Component {
     } = this.state
 
     const enableSubmit = email.length > 0 && code.length > 0
-    if (!true) {
+    if (created) {
       return (
         <DetailComponent handleSubmit={this.handleUpdateInfo} />
       )
@@ -126,50 +126,55 @@ class App extends React.Component {
     return (
       <div className={styles.MainContainer}>
         <LogoBox />
-        <TitleBox title='开始你的小对话' />
-        <div className={styles.FormContainer}>
-          <p>1. 输入你的 V2EX 用户名</p>
-          <input
-            placeholder='用户名'
-            type='text'
-            className={styles.UserNameInput}
-            onChange={this.onUserNameChange}
-            />
-          <p className={nameVerified ? styles.PassText : styles.ErrorText}>{verifyNameTip}</p>
-          <p className={nameVerified ? '' : styles.InactiveText} >
-                2. 输入你的邮箱
-            </p>
-          <input
-            placeholder='email'
-            type='email'
-            disabled={!nameVerified}
-            className={styles.EmailInput}
-            onChange={this.onEmailChange}
-            />
-          <p className={nameVerified && !!email ? '' : styles.InactiveText} >
-                3. 把下面的验证码添加到 V2EX 个人简介 (?)
-            </p>
-          <input
-            placeholder='自动生成验证码'
-            type='text'
-            disabled={!nameVerified || !email}
-            className={styles.CodeInput}
-            value={code}
-            />
-          <p> {this.verifyCodeTip()}</p>
-          <button
-            type='button'
-            disabled={!enableSubmit}
-            onClick={this.handleSubmit}
-            className={(!nameVerified || !email) ? styles.SubmitBtn + ' ' + styles.BtnDisable : styles.SubmitBtn}
-            > 注册
-            </button>
+        <div className={styles.RegistrationContainer}>
+          <TitleBox title='开始你的小对话' />
+          <div className={styles.FormContainer}>
+            <p>1. 输入你的 V2EX 用户名</p>
+            <input
+              placeholder='用户名'
+              type='text'
+              className={styles.UserNameInput}
+              onChange={this.onUserNameChange}
+              />
+            <p className={nameVerified ? styles.PassText : styles.ErrorText}>{verifyNameTip}</p>
+            <p className={nameVerified ? '' : styles.InactiveText} >
+                  2. 输入你的邮箱
+              </p>
+            <input
+              placeholder='email'
+              type='email'
+              disabled={!nameVerified}
+              className={styles.EmailInput}
+              onChange={this.onEmailChange}
+              />
+            <p className={nameVerified && !!email ? '' : styles.InactiveText} >
+                  3. 把下面的验证码添加到 V2EX 个人简介 (?)
+              </p>
+            <input
+              placeholder='自动生成验证码'
+              type='text'
+              disabled={!nameVerified || !email}
+              className={styles.CodeInput}
+              value={code}
+              />
+            <p> {this.verifyCodeTip()}</p>
+            <button
+              type='button'
+              disabled={!enableSubmit}
+              onClick={this.handleSubmit}
+              className={(!nameVerified || !email) ? styles.SubmitBtn + ' ' + styles.BtnDisable : styles.SubmitBtn}
+              > 注册
+              </button>
+          </div>
         </div>
-        <div className={styles.introContainer}>
+        <div className={styles.IntroContainer}>
           <TitleBox title='How does it works?' />
-          <p>小对话每天为你匹配一位 V2EX 好友，然后发送一封包括他个人介绍的邮件到你的邮箱。</p>
-          <p className={styles.CenterText}>· · ·</p>
-          <img className={styles.Screenshot} src={screenshot} />
+          <div className={styles.IntroText}>
+            <p>小对话每天为你匹配一位 V2EX 好友，然后发送一封包括他个人介绍的邮件到你的邮箱。</p>
+          </div>
+          <div className={styles.Screenshot}>
+            <img src={screenshot} />
+          </div>
         </div>
       </div>
     )
