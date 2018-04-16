883e4589d324922cb9e668456289f24edeb23764
Minghe Huang <h.minghe@gmail.com>
2018-04-16 23:45:42 +0800

    Fix typo
    Former-commit-id: be47520cba94d80914fbda206655f7c15613ebc8

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,6 +2,6 @@
 
 docker stop $(docker ps -a -q)
 docker build -f devops/dockerfile.client -t ff-client .
-docker run -p 80:80 443:443 -v /etc/letsencrypt -d ff-client
+docker run -p 80:80 -p 443:443 -v /etc/letsencrypt -d ff-client
 docker build -f devops/dockerfile.server -t ff-server .
 docker run -p 5002:5002 -d ff-server
