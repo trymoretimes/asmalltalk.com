48196e744b48f0cd37c205e69dd80e87bc60024b
Minghe Huang <h.minghe@gmail.com>
2018-04-16 23:59:03 +0800

    fix wrong deloy
    Former-commit-id: 2414b72559f7fd4bbf4246db18d5090fb5a8c3be

--- devops/run.sh
+++ devops/run.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
-set -e
-
-email=h.minghe@gmail.com
-domain=asmalltalk.com
-
-echo "enabling letsencrypt ${domain} with ${email}"
-docker run --rm -p 80:80 -p 443:443 \
-  -v /etc/letsencrypt:/etc/letsencrypt \
-  quay.io/letsencrypt/letsencrypt auth \
-  --standalone -m ${email} --agree-tos \
-    -d ${domain}
-
-echo "renew letsencrypt ${domain} with ${email}"
-docker run --rm -p 80:80 -p 443:443 \
-  -v /etc/letsencrypt:/etc/letsencrypt \
-  quay.io/letsencrypt/letsencrypt renew \
-  --standalone
-
-
 NODE_ENV=production npm run build && nginx -g 'daemon off;'
