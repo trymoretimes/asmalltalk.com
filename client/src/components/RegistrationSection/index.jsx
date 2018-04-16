bc695ef4d715d272b9dde285cf2555793c63f307
Minghe Huang <h.minghe@gmail.com>
2018-04-16 11:19:56 +0800

    refactor
    Former-commit-id: dc3f63b15e2d3b4c26d3609761ae455a0ba2d29f

---
+++ client/src/components/RegistrationSection/index.jsx
@@ -0,0 +1,175 @@
+import React from 'react'
+
+import styles from '../../styles.css'
+import TitleBox from '../TitleBox'
+import api from '../../api'
+import { maybeEmailAddress } from '../../utils'
+
+const FlagText = ({ type, text }) => {
+  const classNames = {
+    error: styles.ErrorText,
+    success: styles.PassText,
+    inactive: styles.InactiveText
+  }
+  return (
+    <p className={classNames[type] || ''}> {text} </p>
+  )
+}
+
+const InputRow = ({ label, disabled, placeholder, onChangeHandler }) => {
+  return (
+    <div>
+      <FlagText type={disabled ? 'inactive' : null} text={label} />
+      <input
+        placeholder={placeholder}
+        type='text'
+        className={styles.UserNameInput}
+        disabled={disabled}
+        onChange={onChangeHandler}
+        />
+    </div>
+  )
+}
+
+class RegistrationSection extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      email: '',
+      code: '',
+
+      usernameIsVerifying: false,
+      usernameIsValid: null // TODO no verified yet
+    }
+
+    this.onUserNameChange = this.onUserNameChange.bind(this)
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  onUserNameChange (evt) {
+    const username = evt.target.value
+
+    this.setState({ username })
+    setTimeout(async () => {
+      const { username } = this.state
+      this.setState({
+        usernameIsVerifying: true,
+        username
+      })
+      const info = await api.getUserProfile({ username })
+      if (info.status === 'found') {
+        this.setState({
+          user: true,
+          usernameIsVerifying: false,
+          usernameIsValid: true
+        }, async () => {
+          await this.getCode()
+        })
+      } else {
+        this.setState({
+          code: '',
+          usernameIsVerifying: false,
+          usernameIsValid: false
+        })
+      }
+    }, 2000)
+  }
+
+  async getCode () {
+    const { username, email } = this.state
+    if (maybeEmailAddress(email)) {
+      const { code } = await api.getCode(username, email)
+      this.setState({ code })
+    }
+  }
+
+  onEmailChange (evt) {
+    const email = evt.target.value
+    if (maybeEmailAddress(email)) {
+      this.setState({ email })
+      setTimeout(async () => {
+        await this.getCode()
+      }, 2000)
+    }
+  }
+
+  verifyCodeTip () {
+    const { verified, loadingCount } = this.state
+    if (verified) {
+      return '✓ 账号验证成功'
+    } else if (loadingCount > 0) {
+      return '验证中' + '.'.repeat(loadingCount % 4)
+    } else {
+      return ''
+    }
+  }
+
+  handleSubmit () {
+    const { code, username, email } = this.state
+    const { onSubmit } = this.props
+    onSubmit({ code, username, email })
+  }
+
+  render () {
+    const {
+      email,
+      usernameIsVerifying,
+      usernameIsValid,
+      code
+    } = this.state
+
+    let verifyTipText = ''
+    let verifyTipType = 'default'
+    if (usernameIsVerifying) {
+      verifyTipText = '正在验证用户名'
+    } else if (usernameIsValid) {
+      verifyTipText = '用户名验证成功'
+      verifyTipType = 'success'
+    } else if (usernameIsValid === false) {
+      verifyTipText = '用户名验证失败'
+      verifyTipType = 'error'
+    }
+
+    const isReady = email.length > 0 && code.length > 0 && usernameIsValid
+    return (
+      <div className={styles.RegistrationContainer}>
+        <TitleBox title='开始你的小对话' />
+        <div className={styles.FormContainer}>
+          <InputRow
+            label='1. 输入你的 V2EX 用户名'
+            placeholder='V2EX 用户名'
+            onChangeHandler={this.onUserNameChange}
+          />
+          <FlagText text={verifyTipText} type={verifyTipType} />
+          <InputRow
+            label='2. 输入你的邮箱'
+            placeholder='email'
+            disabled={!usernameIsValid}
+            onChangeHandler={this.onEmailChange}
+          />
+          <FlagText type={!isReady ? 'inactive' : null} text='3. 把下面的验证码添加到 V2EX 个人简介 (?)' />
+          <input
+            placeholder='自动生成验证码'
+            type='text'
+            disabled={!usernameIsValid || !email}
+            className={styles.CodeInput}
+            value={code}
+            />
+          <p> {this.verifyCodeTip()}</p>
+          <button
+            type='button'
+            disabled={!isReady}
+            onClick={this.handleSubmit}
+            className={isReady ? styles.SubmitBtn : styles.SubmitBtn + ' ' + styles.BtnDisable}
+            > 注册
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RegistrationSection
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom'
 
 import api from './api'
 import styles from './styles.css'
-import { maybeEmailAddress } from './utils'
 import DetailComponent from './components/DetailComponent'
 import LogoBox from './components/LogoBox'
 import AboutSection from './components/AboutSection'
-import TitleBox from './components/TitleBox'
+import RegistrationSection from './components/RegistrationSection'
 
 class App extends React.Component {
   constructor () {
@@ -28,51 +27,8 @@ class App extends React.Component {
       updated: false
     }
 
-    this.onUserNameChange = this.onUserNameChange.bind(this)
-    this.onEmailChange = this.onEmailChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleUpdateInfo = this.handleUpdateInfo.bind(this)
-  }
-
-  verifyCodeTip () {
-    const { verified, loadingCount } = this.state
-    if (verified) {
-      return '✓ 账号验证成功'
-    } else if (loadingCount > 0) {
-      return '验证中' + '.'.repeat(loadingCount % 4)
-    } else {
-      return ''
-    }
-  }
-
-  componentDidMount () {
-  }
-
-  async onUserNameChange (evt) {
-    const username = evt.target.value
-
-    this.setState({ username })
-    setTimeout(async () => {
-      const { username } = this.state
-      this.setState({
-        verifyNameTip: '正在验证用户名',
-        usernameVerifying: true,
-        username
-      })
-      const info = await api.getUserProfile({ username })
-      if (info.status === 'found') {
-        this.setState({
-          usernameVerified: true,
-          verifyNameTip: '✓ 账号有效',
-          usernameVerifying: false
-        })
-      } else {
-        this.setState({
-          verifyNameTip: '无效的用户名',
-          usernameVerifying: false
-        })
-      }
-    }, 1000)
   }
 
   async verifyCode () {
@@ -90,20 +46,8 @@ class App extends React.Component {
     }
   }
 
-  onEmailChange (evt) {
-    const email = evt.target.value
-    if (maybeEmailAddress(email)) {
-      this.setState({ email })
-      setTimeout(async () => {
-        const { username, email } = this.state
-        const { code } = await api.getCode(username, email)
-        this.setState({ code })
-      }, 500)
-    }
-  }
-
-  async handleSubmit () {
-    const { username, email, code } = this.state
+  async handleSubmit (payload) {
+    const { username, email, code } = payload
     const users = await api.query({ email, username })
     if (users.length > 0) {
       this.setState({ created: true, userId: users[0]._id })
@@ -127,16 +71,8 @@ class App extends React.Component {
   }
 
   render () {
-    const {
-      code,
-      email,
-      usernameVerified,
-      verifyNameTip,
-      created,
-      updated
-    } = this.state
+    const { created, updated } = this.state
 
-    const enableSubmit = email.length > 0 && code.length > 0
     if (created) {
       return (
         <DetailComponent handleSubmit={this.handleUpdateInfo} />
@@ -150,48 +86,8 @@ class App extends React.Component {
     return (
       <div className={styles.MainContainer}>
         <LogoBox />
-        <div className={styles.RegistrationContainer}>
-          <TitleBox title='开始你的小对话' />
-          <div className={styles.FormContainer}>
-            <p>1. 输入你的 V2EX 用户名</p>
-            <input
-              placeholder='用户名'
-              type='text'
-              className={styles.UserNameInput}
-              onChange={this.onUserNameChange}
-              />
-            <p className={usernameVerified ? styles.PassText : styles.ErrorText}>{verifyNameTip}</p>
-            <p className={usernameVerified ? '' : styles.InactiveText} >
-                  2. 输入你的邮箱
-              </p>
-            <input
-              placeholder='email'
-              type='email'
-              disabled={!usernameVerified}
-              className={styles.EmailInput}
-              onChange={this.onEmailChange}
-              />
-            <p className={usernameVerified && !!email ? '' : styles.InactiveText} >
-                  3. 把下面的验证码添加到 V2EX 个人简介 (?)
-              </p>
-            <input
-              placeholder='自动生成验证码'
-              type='text'
-              disabled={!usernameVerified || !email}
-              className={styles.CodeInput}
-              value={code}
-              />
-            <p> {this.verifyCodeTip()}</p>
-            <button
-              type='button'
-              disabled={!enableSubmit}
-              onClick={this.handleSubmit}
-              className={(!usernameVerified || !email) ? styles.SubmitBtn + ' ' + styles.BtnDisable : styles.SubmitBtn}
-              > 注册
-            </button>
-          </div>
-          <AboutSection />
-        </div>
+        <RegistrationSection onSubmit={this.handleSubmit} />
+        <AboutSection />
       </div>
     )
   }
