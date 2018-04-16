e3b727f183f4bc989122b6964aa7fb43e561d770
Minghe Huang <h.minghe@gmail.com>
2018-04-17 01:04:07 +0800

    build -> stop -> start
    Former-commit-id: 6994a6ed32adebcc6ad10f8c5d7cf907bb84955c

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,8 +5,6 @@ set -e
 email=h.minghe@gmail.com
 domain=asmalltalk.com
 
-docker stop $(docker ps -a -q)
-
 # echo "enabling letsencrypt ${domain} with ${email}"
 # docker run --rm -p 80:80 -p 443:443 \
 #   -v /etc/letsencrypt:/etc/letsencrypt \
@@ -21,6 +19,9 @@ docker stop $(docker ps -a -q)
 #   --standalone
 
 docker build -f devops/dockerfile.client -t ff-client .
-docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -d ff-client
 docker build -f devops/dockerfile.server -t ff-server .
+
+docker stop $(docker ps -a -q)
+
 docker run -p 5002:5002 -d ff-server
+docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -d ff-client
