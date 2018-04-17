d0904c7882d444a5072dfba058875bd47434e41a
Minghe Huang <h.minghe@gmail.com>
2018-04-17 10:14:04 +0800

    fix format
    Former-commit-id: 24d5d79574a7832f65d298a294f4702501cddbbc

--- server/src/jobs/mailer.js
+++ server/src/jobs/mailer.js
@@ -67,48 +67,46 @@ class Mailer {
       replyTo: matchee.email,
       subject: `小对话：为你推荐 V2EX 用户 ${matchee.name}`,
       text: `
-          Hi ${matcher.username}，\n
-          今天为你推荐的 V2EX 用户是 ${matchee.username}，以下是 Linus 的个人简介：\n\n
+Hi ${matcher.username}，<br>
+今天为你推荐的 V2EX 用户是 ${matchee.username}，以下是 Linus 的个人简介：<br><br>
 
-          Email: ${matchee.email} \n
-          个人网站：\n
-          Twitter:\n
-          知乎账号：\n
-          微信账号：\n
-          V2EX 个人档案：https://www.v2ex.com/member/${matchee.username}\n\n
+Email: ${matchee.email} \r\n
+个人网站：\r\n
+Twitter:\r\n
+知乎账号：\r\n
+微信账号：\r\n
+V2EX 个人档案：https://www.v2ex.com/member/${matchee.username}\r\n\r\n
 
-          Linus 擅长的事物：${matchee.canHelp}\n
-          Linus 希望得到帮助的事物：${matchee.needHelp}\n
+Linus 擅长的事物：${matchee.canHelp}\r\n
+Linus 希望得到帮助的事物：${matchee.needHelp}\r\n
 
-          想认识他？直接回复这封邮件跟他 say hi 吧。 \n
+想认识他？直接回复这封邮件跟他 say hi 吧。 \r\n
 
-          小对话 \n
+小对话 \r\n
 
-          更新你的档案：https://www.asmalltalk.com \n
-          退订：https://www.asmalltalk.com \n
+更新你的档案：https://www.asmalltalk.com \r\n
+退订：https://www.asmalltalk.com \r\n
       `,
       html: `
-          <p>
-          Hi ${matcher.username}， \n
-          今天为你推荐的 V2EX 用户是 ${matchee.username}，以下是 Linus 的个人简介： \n\n
+Hi ${matcher.username}， <br>
+今天为你推荐的 V2EX 用户是 ${matchee.username}，以下是 Linus 的个人简介： <br><br>
 
-          Email: ${matchee.email} \n
-          个人网站： \n
-          Twitter:\n
-          知乎账号：\n
-          微信账号：\n
-          V2EX 个人档案：https://www.v2ex.com/member/${matchee.username}\n\n
+Email: ${matchee.email} <br>
+个人网站： <br>
+Twitter:<br>
+知乎账号：<br>
+微信账号：<br>
+V2EX 个人档案：https://www.v2ex.com/member/${matchee.username}<br><br>
 
-          Linus 擅长的事物：${matchee.canHelp}\n
-          Linus 希望得到帮助的事物：${matchee.needHelp}\n
+Linus 擅长的事物：${matchee.canHelp}<br>
+Linus 希望得到帮助的事物：${matchee.needHelp}<br>
 
-          想认识他？直接回复这封邮件跟他 say hi 吧。\n\n
+想认识他？直接回复这封邮件跟他 say hi 吧。<br><br>
 
-          小对话\n\n
+小对话<br><br>
 
-          更新你的档案：https://www.asmalltalk.com\n\n
-          退订：https://www.asmalltalk.com\n
-          </p>
+更新你的档案：https://www.asmalltalk.com<br><br>
+退订：https://www.asmalltalk.com<br>
           `
     }
 
