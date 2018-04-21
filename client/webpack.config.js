abd56729643da6d97ea6233a34f6aa5e61f56794
Minghe Huang <h.minghe@gmail.com>
2018-04-21 08:51:03 +0800

    build bootstrap css into bundle
    Former-commit-id: 672e969d2bc2f109933780274626d936659536bb

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: /site\.css$|react-select\.css|bootstrap\.css$|fixed-data-table\.min\.css$|rodal\.css$|plugin\.css$/,
+        exclude: /site\.css$|bootstrap\.css$/,
         use: [
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
