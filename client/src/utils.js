7bcfe3227839fce6bdb2771d961dd475dcc4aaee
Minghe Huang <h.minghe@gmail.com>
2018-04-22 15:57:07 +0800

    revert accidend remove
    Former-commit-id: 0f70582fe08b77280794200a75b8f6a11ba0be6f

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,3 +2,10 @@ export function maybeEmailAddress (email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(email)
 }
+
+export function getSiteAndUserId (str) {
+  if (str.includes('/')) {
+     return str.split('/')
+  }
+  return ['v2ex', str]
+}
