0cf9ffba90200cd6e8aa566d28c5d3c70e58c477
Minghe Huang <h.minghe@gmail.com>
2018-04-23 09:41:47 +0800

    fix path issue
    Former-commit-id: 4bc2286d59a3ada929505c97fd823c0057126de8

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -18,18 +18,18 @@ domain=asmalltalk.com
 #   quay.io/letsencrypt/letsencrypt renew \
 #   --standalone
 
-cd ~/ff/server && docker build -f devops/dockerfile -t ff-server .
-cd ~/ff/client && docker build -f devops/dockerfile -t ff-client .
-cd ~/ff/services/matcher && docker build -f devops/dockerfile -t ff-matcher .
-cd ~/ff/services/mailer && docker build -f devops/dockerfile -t ff-mailer .
-cd ~/ff/services/jobs && docker build -f devops/dockerfile -t ff-jobs .
+cd ~/Codes/asmalltalk.com/server && docker build -f devops/dockerfile -t ff-server .
+cd ~/Codes/asmalltalk.com/client && docker build -f devops/dockerfile -t ff-client .
+cd ~/Codes/asmalltalk.com/services/matcher && docker build -f devops/dockerfile -t ff-matcher .
+cd ~/Codes/asmalltalk.com/services/mailer && docker build -f devops/dockerfile -t ff-mailer .
+cd ~/Codes/asmalltalk.com/services/jobs && docker build -f devops/dockerfile -t ff-jobs .
 
-docker stop $(docker ps -a -q)
-
-docker run -p 5002:5002 -d ff-server
-# wait for server start
-sleep 20
-docker run -d ff-matcher
-docker run -d ff-mailer
-docker run -d ff-jobs
-docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -d ff-client
+# docker stop $(docker ps -a -q)
+#
+# docker run -p 5002:5002 -d ff-server
+# # wait for server start
+# sleep 20
+# docker run -d ff-matcher
+# docker run -d ff-mailer
+# docker run -d ff-jobs
+# docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -d ff-client
