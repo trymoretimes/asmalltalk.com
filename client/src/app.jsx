a9f68f32d142b065bfdf3a47efe0eed35dd8f1e2
Minghe Huang <h.minghe@gmail.com>
2018-04-26 22:18:21 +0800

    setup 404 page
    Former-commit-id: 1912c64d1b7a3b7be0ec73edc577cd8411342bf5

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import Home from './components/App'
 import User from './components/User'
+import NotFound from './components/NotFound'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 ReactDOM.render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ App }>
-      <IndexRoute component={ App } />
+    <Route path="/" component={ Home }>
+      <IndexRoute component={ Home } />
     </Route>
     <Route path="users/:id" component={ User } />
+    <Route path='*' component={ NotFound } />
   </Router>
 ), document.body)
