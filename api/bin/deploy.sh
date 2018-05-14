499a5afc8db1dcb4724f115fa2e45cb14fb69062
Minghe Huang <h.minghe@gmail.com>
2018-05-14 22:57:52 +0800

    fix typo

--- api/bin/deploy.sh
+++ api/bin/deploy.sh
@@ -7,7 +7,7 @@ stage=$1
 CURRENT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 ROOT_DIR=$(cd "${CURRENT_DIR}/.."; pwd -P)
 
-${ROOT_DIR}/node_modules/sls deploy -v \
+${ROOT_DIR}/node_modules/.bin/sls deploy -v \
   --stage ${stage} \
   --SENDGRID_API_KEY ${SENDGRID_API_KEY} \
   --ASMALLTALK_EMAIL ${ASMALLTALK_EMAIL} \
