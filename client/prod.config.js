f34b2241ad8204fa6de6ed5ff9d12074ad5b15ff
Minghe Huang <h.minghe@gmail.com>
2018-05-20 01:37:05 +0800

    update url

--- client/prod.config.js
+++ client/prod.config.js
@@ -1,1 +1,1 @@
-export const API_HOST = 'https://1myh7f7eu9.execute-api.us-east-1.amazonaws.com/production'
+export const API_HOST = 'https://askv4zomve.execute-api.us-east-1.amazonaws.com/production'
