694588cc7fcea57d3eb17d045b2344b68d48e888
Minghe Huang <h.minghe@gmail.com>
2018-05-21 18:31:05 +0800

    support github now

--- client/src/components/AboutSection/index.jsx
+++ client/src/components/AboutSection/index.jsx
@@ -8,7 +8,7 @@ const AboutSection = () => (
   <div className={styles.IntroContainer}>
     <TitleBox title='什么是小对话' />
     <div className={styles.IntroText}>
-      <p>小对话每天为你匹配一位 V2EX 好友，然后发送一封包括他个人介绍的邮件到你的邮箱。</p>
+      <p>小对话每天为你匹配一位好友，然后发送一封包括他个人介绍的邮件到你的邮箱。</p>
     </div>
     <div className={styles.Screenshot}>
       <img src={screenshot} />
